b460ed07b48ecb0a9ddb6ce62c48b3b9cfd79045
Abishek3344 <tathabi8@gmail.com>
2021-04-20 11:20:31 +0530

    update project

--- src/model/Project.java
+++ src/model/Project.java
@@ -117,6 +117,44 @@ public class Project {
 		 
 				return output;
 		 } 
+		
+		public String updateProject(String ID, String name, String details, String fund)
+
+		{
+				String output = "";
+			
+				try 
+				{
+					Connection con = connect();
+					if (con == null) {
+					return "Error while connecting to the database for updating.";
+				}
+				
+					// create a prepared statement
+					String query = "UPDATE project SET projectName=?,projectDetails=?,projectFund=? WHERE projectID=?";
+				
+					PreparedStatement preparedStmt = con.prepareStatement(query);
+				
+					// binding values
+					preparedStmt.setString(1, name);
+					preparedStmt.setString(2, details);
+					preparedStmt.setDouble(3, Double.parseDouble(fund));
+					preparedStmt.setInt(5, Integer.parseInt(ID));
+				
+					// execute the statement
+					preparedStmt.execute();
+					con.close();
+				
+					output = "Updated successfully";
+			} 
+				catch (Exception e) 
+				{
+					output = "Error while updating the project.";
+					System.err.println(e.getMessage());
+				}
+			
+				return output;
+		}
 
 
 }

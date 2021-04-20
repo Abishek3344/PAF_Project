08121eeaad80b4695a8f93f1692f8fd3ed854493
Abishek3344 <tathabi8@gmail.com>
2021-04-20 18:34:34 +0530

    Align code

--- src/model/Project.java
+++ src/model/Project.java
@@ -22,6 +22,64 @@ public class Project {
 				return con;
 		}
 		
+		
+		public String readProjects()
+		 {
+				String output = "";
+				
+				try
+				{
+					Connection con = connect();
+		 
+					if (con == null)
+					{return "Error while connecting to the database for reading."; }
+					// Prepare the html table to be displayed
+					output = "<table border='1'><tr><th>Project Code</th><th>Project Name</th>" +
+							"<th>Project Details</th>" +
+							"<th>ProjectFund</th>" +
+							"<th>Update</th><th>Remove</th></tr>";
+
+					String query = "select * from project";
+					Statement stmt = con.createStatement();
+					ResultSet rs = stmt.executeQuery(query);
+		 
+					// iterate through the rows in the result set
+					while (rs.next())
+					{
+						String projectID = Integer.toString(rs.getInt("projectID"));
+						String projectName = rs.getString("projectName");
+						String projectDetails = rs.getString("projectDetails");
+						String projectFund = Double.toString(rs.getDouble("projectFund"));
+		 
+						// Add into the html table
+						output += "<tr><td>" + projectID + "</td>";
+						output	+= "<td>" + projectName + "</td>";
+						output += "<td>" + projectDetails + "</td>";
+						output += "<td>" + projectFund + "</td>";
+		 
+						// buttons
+						output += "<td><input name='btnUpdate' type='button' value='Update'class='btn btn-secondary'></td>"
+								+ "<td><form method='post' action='items.jsp'>"
+								+ "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
+								+ "<input name='itemID' type='hidden' value='" + projectID
+								+ "'>" + "</form></td></tr>";
+					}
+		 
+					con.close();
+					
+					// Complete the html table
+					output += "</table>";
+				}
+				catch (Exception e)
+				{
+					output = "Error while reading the items.";
+					System.err.println(e.getMessage());
+				}
+		 
+				return output;
+		 }
+		
+		
 		public String insertProject(String name, String details, String fund)
 		{
 				String output = "";

21b980372bed5adbac76e0e0399732506a606734
Thanusha Sritharan <srithanu388@gmail.com.com>
2021-04-22 21:51:17 +0530

    Get all method implemented for product service

--- src/com/ProductService.java
+++ src/com/ProductService.java
@@ -1,5 +1,28 @@
 package com;
 
-public class ProductService {
+//For REST Service
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+//For JSON
+import com.google.gson.*;
 
+import model.Product;
+
+//For XML
+import org.jsoup.*;
+import org.jsoup.parser.*;
+import org.jsoup.nodes.Document;
+
+@Path("/Products") 
+
+public class ProductService {
+	Product productObj = new Product();
+	
+	@GET
+	@Path("/getAll")
+	@Produces(MediaType.TEXT_HTML)
+	public String readProjects()
+	 {
+		return productObj.getProducts();
+	 } 
 }
--- src/model/Product.java
+++ src/model/Product.java
@@ -21,5 +21,64 @@ public class Product {
 
 			return con;
 	}
+	
+	public String getProducts()
+	 {
+			String output = "";
+			
+			try
+			{
+				Connection con = connect();
+	 
+				if (con == null)
+				{return "Error while connecting to database for reading...."; }
+				// Prepare the html table to be displayed
+				output = "<table border='1'><tr><th>Project Code</th><th>Product Name</th>" +
+						"<th>Description</th>" +
+						"<th>Product Type</th>" +
+						"<th>Amount</th>" +
+						"<th>Update</th><th>Remove</th></tr>";
+
+				String query = "select * from product";
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(query);
+	 
+				// iterate through the rows in the result set
+				while (rs.next())
+				{
+					String id = Integer.toString(rs.getInt("ID"));
+					String name = rs.getString("Name");
+					String des = rs.getString("Description");
+					String type = rs.getString("Type");
+					String amount = Double.toString(rs.getDouble("Amount"));
+	 
+					// Add into the html table
+					output += "<tr><td>" + id + "</td>";
+					output	+= "<td>" + name + "</td>";
+					output += "<td>" + des + "</td>";
+					output += "<td>" + type + "</td>";
+					output += "<td>" + amount + "</td>";
+	 
+					// buttons
+					output += "<td><input name='btnUpdate' type='button' value='Update'class='btn btn-secondary'></td>"
+							+ "<td><form method='post' action='items.jsp'>"
+							+ "<input name='btnRemove' type='submit' value='Remove'class='btn btn-danger'>"
+							+ "<input name='itemID' type='hidden' value='" + id
+							+ "'>" + "</form></td></tr>";
+				}
+	 
+				con.close();
+				
+				// Complete the html table
+				output += "</table>";
+			}
+			catch (Exception e)
+			{
+				output = "Error while reading the items.";
+				System.err.println(e.getMessage());
+			}
+	 
+			return output;
+	 }
 
 }

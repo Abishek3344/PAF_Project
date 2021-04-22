8f9b1df06049f38e5bae2728136ec50498f10667
Thanusha Sritharan <srithanu388@gmail.com.com>
2021-04-22 21:47:14 +0530

    Product service data base connection

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,5 +1,25 @@
 package model;
 
+import java.sql.*; 
+
 public class Product {
+	
+	// DB connection
+	private Connection connect()
+	{
+			Connection con = null;
+			
+			try
+			{
+				Class.forName("com.mysql.jdbc.Driver");
+
+				//DBServer/DBName, username, password
+				con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf_product_db", "root", "");
+			}
+			catch (Exception e)
+			{e.printStackTrace();}
+
+			return con;
+	}
 
 }

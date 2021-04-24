9ffd3648ed6237d62e648e192376ffb9935421de
good <jathursiniperinpanathan19@gmail.com>
2021-04-25 00:21:21 +0530

    Server commit

--- PaymentService/src/model/Payment.java
+++ PaymentService/src/model/Payment.java
@@ -14,7 +14,7 @@ public class Payment {
 	 Class.forName("com.mysql.jdbc.Driver"); 
 	 con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:8081/testdb", "root", ""); 
 	 //For testing
-	 System.out.print("Successfully connected to dataBase"); 
+	 System.out.print("Successfully connected to dataBase and server "); 
 	 } 
 	 catch(Exception e) 
 	 { 
